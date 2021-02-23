df411490f3ea541cc2070c930b07c8632660d54e
Adem Atici <adematicis41@gmail.com>
2021-02-23 13:42:11 +0300

    SplashScreen tamamlandi

---
+++ app/src/main/java/com/adematici/turkcesozlukveatasozleri/SplashScreen.kt
@@ -0,0 +1,34 @@
+package com.adematici.turkcesozlukveatasozleri
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },1000)
+    }
+
+}
